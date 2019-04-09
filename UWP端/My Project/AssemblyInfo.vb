c8f5c3a318585eb73b87c1afd0309783696e22ec
锹甲阿尔法 <zhangtianfu0@126.com>
2019-04-09 14:35:27 +0800

    NuGet发布0

--- UWP端/My Project/AssemblyInfo.vb
+++ UWP端/My Project/AssemblyInfo.vb
@@ -8,11 +8,11 @@ Imports System.Runtime.InteropServices
 
 '查看程序集特性的值
 
-<Assembly: AssemblyTitle("UWP端")> 
-<Assembly: AssemblyDescription("")> 
-<Assembly: AssemblyCompany("")> 
-<Assembly: AssemblyProduct("UWP端")> 
-<Assembly: AssemblyCopyright("Copyright ©  2019")> 
+<Assembly: AssemblyTitle("ProcessForUWP.UWP端")>
+<Assembly: AssemblyDescription("这是对System.Diagnostics.Process类的高仿。我们知道，在UWP中一般是不允许对此类直接的使用的。而通过本高仿类可以间接地使用此类，实现用UWP启动桌面进程，并与其进行标准输入输出的交互。此项目分两个包，此包是UWP端，依赖桌面端")>
+<Assembly: AssemblyCompany("翁悸会")>
+<Assembly: AssemblyProduct("ProcessForUWP")>
+<Assembly: AssemblyCopyright("")>
 <Assembly: AssemblyTrademark("")> 
 
 ' 程序集的版本信息由下列四个值组成: 
